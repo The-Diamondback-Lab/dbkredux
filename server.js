316ea99df283a4a7cf5630ae7618de703a3a9c0f
Akshay Guthal <akguthal@gmail.com>
2019-02-21 20:10:45 -0500

    test compression package

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+const compression = require('compression')  
  
 // With express
 const express = require('express')
 app.prepare().then(() => {
-  express().use(handler).listen(3000)
+  express().use(compression()).use(handler).listen(3000)
 })

5eb05382ea30a3e726a4f256b9adb32ef900c96a
fishnal <vpatel14@terpmail.umd.edu>
2019-12-08 16:58:57 -0500

    Deploying nextjs on aws beanstalk via travis
    https://medium.com/commutatus/how-to-deploy-next-js-app-on-aws-elastic-beanstalk-via-travis-333f66fe3102

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // server.js
-const next = require('next')
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const handler = routes.getRequestHandler(app)
-const compression = require('compression')  
- 
-// With express
-const express = require('express')
-app.prepare().then(() => {
-  const server = express()
-  server.use(compression())
-  server.use(handler).listen(3000)
-})
+const express = require('express');
+const next = require('next');
+const port = process.env.PORT || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(por, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  }).catch((exception) => {
+    console.error(exception.stack);
+    process.exit(1);
+  });

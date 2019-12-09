7203976d47ffc97e74683cb7ee831dae982af159
fishnal <vpatel14@terpmail.umd.edu>
2019-12-08 23:46:56 -0500

    commented out target prop in next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
-    ? {} // We're never in "production server" phase when in development mode 
+    ? {} // We're never in "production server" phase when in development mode
     : !process.env.NOW_REGION
-      ? require('next/constants') // Get values from `next` package when building locally 
+      ? require('next/constants') // Get values from `next` package when building locally
       : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
 module.exports = (phase, { defaultConfig }) => {
@@ -15,7 +15,7 @@ module.exports = (phase, { defaultConfig }) => {
   const withSass = require('@zeit/next-sass')
 
   return withSass(withCSS({
-    target: 'serverless',
+    // target: 'serverless',
     webpack: function (config) {
       config.module.rules.push({
         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,

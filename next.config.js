d78cae84612e632b6b00da1c58a46c88a9f58f01
Vishal Patel <vishalpatel3199@gmail.com>
2020-05-07 23:26:28 -0400

    Next.js config has css and sass loaders

--- next.config.js
+++ next.config.js
@@ -1,23 +1,42 @@
+const withSass = require('@zeit/next-sass')
+const withCss = require('@zeit/next-css')
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
+const useBackendServers = (process.env.PRODUCTION_BACKEND || '').toLowerCase() === 'true'
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || process.env.PRODUCTION_BACKEND.toLowerCase() === 'true') {
-    return {
-      env: {
-        API_URL: 'http://localhost:8080',
-        WP_URL: 'http://localhost/wordpress'
-      }
+  let env = null
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || useBackendServers) {
+    env = {
+      API_URL: 'http://localhost:8080',
+      WP_URL: 'http://localhost/wordpress'
     }
   } else {
     if (phase !== PHASE_PRODUCTION_BUILD) {
       console.warn(`Unexpected NextJS phase: ${phase}`)
     }
 
-    return {
-      env: {
-        API_URL: 'https://api.dbknews.com',
-        WP_URL: 'https://wp.dbknews.com'
-      }
+    env = {
+      API_URL: 'https://api.dbknews.com',
+      WP_URL: 'https://wp.dbknews.com'
     }
   }
+
+  return withCss(withSass({
+    env,
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            name: '[name].[ext]'
+          }
+        }
+      })
+
+      return config
+    }
+  }))
 }

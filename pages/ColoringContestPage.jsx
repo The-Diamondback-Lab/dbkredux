7c5882229f53fc6495b6ed51f885b198795b4c2b
Vishal Patel <vishalpatel3199@gmail.com>
2020-07-02 14:33:00 -0400

    Route for coloring contest page

---
+++ pages/ColoringContestPage.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import Head from 'next/head'
+
+export default class ColoringContestPage extends React.Component {
+  render() {
+    return <React.Fragment>
+      <Head>
+        <title key='title'>Coloring Contest</title>
+        <meta key='og:title' property='og:title' content='Coloring Contest' />
+        <meta key='viewport' name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <main className='page custom-page'>
+        <div className='container-narrow'>
+          <h1 dangerouslySetInnerHTML={{ __html: 'Coloring Contest' }} />
+        </div>
+        <div className='container-narrow flex'>
+          <div className='left-rail'>
+            <hr />
+            <div id='page-content'>
+              Foobar
+            </div>
+          </div>
+        </div>
+      </main>
+    </React.Fragment>
+  }
+}
--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ routes
   .add({ name: 'Category Page', pattern: '/category/:categorypath+', page: 'CategoryPage' })
   .add({ name: 'Author Page', pattern: '/author/:authorId', page: 'AuthorPage' })
   .add({ name: 'Search Results', pattern: '/search', page: 'SearchResults' })
+  .add({ name: 'Coloring Contest Page', pattern: '/coloring-contest', page: 'ColoringContestPage' })
   .add({ name: 'Custom Page', pattern: '/:pageId', page: 'CustomPage' })
   .add({ name: 'Preview', pattern: '/_preview/:postId/:wpnonce', page: 'ArticlePreview' })
   .add({ name: 'SiteMap', pattern: '/sitemap.xml', page: 'ArticlePreview' })
